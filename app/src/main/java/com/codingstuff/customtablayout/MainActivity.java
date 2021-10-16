75f971ff0daa99a3e2f7d397454d652c2acaad22
codingstuff070 <codingstuff070@gmail.com>
2021-10-16 19:40:31 +0530

    CustomTabLayout

---
+++ app/src/main/java/com/codingstuff/customtablayout/MainActivity.java
@@ -0,0 +1,58 @@
+package com.codingstuff.customtablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private MyFragmentAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager2);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Sign In"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new MyFragmentAdapter(fragmentManager , getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/codingstuff/customtablayout/MyFragmentAdapter.java
@@ -0,0 +1,30 @@
+package com.codingstuff.customtablayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MyFragmentAdapter extends FragmentStateAdapter {
+
+
+
+    public MyFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position == 1){
+            return new SignUpFragment();
+        }
+        return new SignInFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
